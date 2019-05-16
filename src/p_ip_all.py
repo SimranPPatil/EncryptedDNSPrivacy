13a3aa43ce7ba2a1af882551aaf3668b893e0671
sppatil2 <sppatil2@illinois.edu>
2019-05-16 08:14:01 -0700

    scripts

--- src/p_ip_all.py
+++ src/p_ip_all.py
@@ -1,5 +1,7 @@
 import sys
-# neater output data_out_*
+# cat ../output/data_out_* > out4.txt   
+# run on out4.txt
+# cat ../output/mpdomainsandsites_out* > out3.txt
 
 if len(sys.argv) != 2:
     print("Enter in the format: python3 parse_shards.py filename")
@@ -9,27 +11,32 @@ ipset_to_domain = dict()
 with open(sys.argv[1]) as f:
     for line in f:
         try:
-            ips = line.split("{")[1].split("}")[0].split(",")
-            domain = line.split(",")[0]
+            ips = line.split("}")[2].split("{")[1].split(",")
+            domain = line.split("}")[2].split("{")[0].split(",")[1]
+            print(domain, ips)
             ipset = set()
             for ip in ips:
                 print(ip)
+                ip = ip.replace("'", "")
+                ip = ip.replace(" ", "")
+                print(ip)
                 ipset.add(ip)
+            print(ipset)
             ipset = frozenset(ipset)
-            #print(ipset)
             ipset_to_domain.setdefault(ipset, set()).add(domain)
         except Exception as e:
             print("Exception: ", e)
             exc_type, _, exc_tb = sys.exc_info()
             print(exc_type, exc_tb.tb_lineno, "\n\n")
 
+
 count = 0
-with open("matchingips.txt", "w+") as f:
+with open("matchingipsall.txt", "w+") as f:
     for ipset in ipset_to_domain:
         if len(ipset_to_domain[ipset]) > 1:
             line = str(ipset) + "," + str(ipset_to_domain[ipset]) + "\n"
             f.write(line)
-            # print(ipset, ipset_to_domain[ipset], len(ipset_to_domain[ipset]))
+            print(ipset, ipset_to_domain[ipset])
             count += 1
 
 print("instances: ", count)
