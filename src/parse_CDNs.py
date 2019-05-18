0364108113260b54b38b6182ca43158ce2ae1c91
sppatil2 <sppatil2@illinois.edu>
2019-05-17 19:22:07 -0700

    fixing parse cdns

--- src/parse_CDNs.py
+++ src/parse_CDNs.py
@@ -17,7 +17,7 @@ with open(sys.argv[1]) as f:
             try:
                 cdnset = set()
                 sites = line.split("}")[0].split("{")[1].split(",")
-                cdns = line.split("}")[3].split("{")[1].split(",")
+                cdns = line.split("}")[-2].split("{")[1].split(",")
                 print(sites, cdns)
                 for cdn in cdns:
                     cdnset.add(cdn)
