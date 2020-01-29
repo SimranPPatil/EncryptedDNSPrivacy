9bd1e8c49349501efe34f84c76d0e0b75372abf2
sppatil2 <sppatil2@illinois.edu>
2020-01-29 01:11:47 -0600

    domain freq renderer to be tested on bigger dataset

--- src/parser.go
+++ src/parser.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	runtime "github.com/banzaicloud/logrus-runtime-formatter"
+	"github.com/go-echarts/go-echarts/charts"
 	log "github.com/sirupsen/logrus"
 	"github.com/teamnsrg/mida/types"
 )
@@ -30,8 +31,8 @@ func init() {
 
 func main() {
 
-	if len(os.Args) != 2 {
-		log.Error("Usage: ./parser path/to/folder_with_sites")
+	if len(os.Args) != 3 {
+		log.Error("Usage: ./parser path/to/folder_with_sites 10-01-2019")
 		return
 	}
 
@@ -39,6 +40,7 @@ func main() {
 
 	filenameChan := make(chan string)
 	resultChan := make(chan parsedData)
+	singlefreqChan := make(chan string)
 
 	rootPath := os.Args[1]
 
@@ -46,15 +48,23 @@ func main() {
 
 	var wg sync.WaitGroup
 	var owg sync.WaitGroup
+	var fwg sync.WaitGroup
 
 	now := time.Now().Format("01-02-2006")
 	last := strings.Split(rootPath, "/")
+
+	singlefreq := map[string]int{}
+	allfreq := map[string]int{}
+
 	owg.Add(1)
-	go output(resultChan, now+"_"+last[len(last)-1]+"_output.json", &owg)
+	go output(resultChan, now+"_"+last[len(last)-1]+"_output.json", &owg, &allfreq)
+
+	fwg.Add(1)
+	go frequency(singlefreqChan, &fwg, &singlefreq)
 
 	for i := 0; i < WORKERS; i++ {
 		wg.Add(1)
-		go worker(filenameChan, resultChan, &wg)
+		go worker(filenameChan, resultChan, singlefreqChan, &wg)
 	}
 
 	dirs, err := ioutil.ReadDir(rootPath)
@@ -64,6 +74,7 @@ func main() {
 	}
 
 	for _, dir := range dirs {
+
 		pathSub := path.Join(rootPath, dir.Name())
 		subdirs, err := ioutil.ReadDir(pathSub)
 		if err != nil {
@@ -79,16 +90,44 @@ func main() {
 	close(filenameChan)
 	wg.Wait()
 
+	close(singlefreqChan)
+	fwg.Wait()
+
 	close(resultChan)
 	owg.Wait()
+
+	// get domains from allfreq and plot for both frequencies for these domain
+	single := []int{}
+	multiple := []int{}
+	domains := []string{}
+
+	bar := charts.NewBar()
+	for domain := range allfreq {
+		_, found := singlefreq[domain]
+		if found {
+			domains = append(domains, domain)
+			single = append(single, singlefreq[domain])
+			multiple = append(multiple, allfreq[domain])
+		}
+	}
+	bar.SetGlobalOptions(charts.TitleOpts{Title: "Domain frequency over time comparison"})
+	bar.AddXAxis(domains).AddYAxis("single", single).AddYAxis("over time", multiple)
+	graph, err := os.Create("bar.html")
+	if err != nil {
+		log.Error("plotting error")
+	}
+	bar.Render(graph)
 
 	log.Info("End")
 }
 
-func worker(filenameChan chan string, resultChan chan parsedData, wg *sync.WaitGroup) {
+func worker(
+	filenameChan chan string,
+	resultChan chan parsedData,
+	singlefreqChan chan string,
+	wg *sync.WaitGroup) {
 
 	for fName := range filenameChan {
-
 		data, e := ioutil.ReadFile(fName)
 		if e != nil {
 			fmt.Printf("File error: %v\n", e)
@@ -127,6 +166,11 @@ func worker(filenameChan chan string, resultChan chan parsedData, wg *sync.WaitG
 				}
 				LoadDomain := u.Host
 
+				FileInfo, _ := os.Stat(fName)
+				if FileInfo.ModTime().Format("01-06-2006") == os.Args[2] {
+					singlefreqChan <- LoadDomain
+				}
+
 				pd := parsedData{
 					RequestID:    RequestID.String(),
 					LoadURL:      LoadURL,
@@ -144,7 +188,28 @@ func worker(filenameChan chan string, resultChan chan parsedData, wg *sync.WaitG
 	wg.Done()
 }
 
-func output(resultChan chan parsedData, ofName string, owg *sync.WaitGroup) {
+func frequency(
+	singlefreqChan chan string,
+	fwg *sync.WaitGroup,
+	singlefreq *map[string]int) {
+
+	for domain := range singlefreqChan {
+		_, found := (*singlefreq)[domain]
+		if found == true {
+			(*singlefreq)[domain]++
+		} else {
+			(*singlefreq)[domain] = 1
+		}
+	}
+
+	fwg.Done()
+}
+
+func output(
+	resultChan chan parsedData,
+	ofName string,
+	owg *sync.WaitGroup,
+	allfreq *map[string]int) {
 
 	f, err := os.Create(ofName)
 	if err != nil {
@@ -158,6 +223,13 @@ func output(resultChan chan parsedData, ofName string, owg *sync.WaitGroup) {
 		if err != nil {
 			log.Error(err)
 			continue
+		}
+
+		_, found := (*allfreq)[result.LoadDomain]
+		if found == true {
+			(*allfreq)[result.LoadDomain]++
+		} else {
+			(*allfreq)[result.LoadDomain] = 1
 		}
 	}
 
