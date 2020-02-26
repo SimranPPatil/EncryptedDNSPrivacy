d4e802edb1a4fe293cba111fca9ea4228135af79
sppatil2 <sppatil2@illinois.edu>
2020-02-25 19:36:26 -0600

    delected

--- src/shard.py
+++
@@ -1,9 +0,0 @@
-with open("../input/1m-resources.csv", "r") as fp:
-    for line in fp:
-        row = line.split(',')
-        idx = int(row[0])%100
-        print(idx)
-        fname = "shards/out_" + str(idx) + ".csv"
-        with open(fname, "a+") as outfile:
-            outfile.write(line) 
-
--- src/test.py
+++
@@ -1,6 +0,0 @@
-import shelve
-from pprint import pprint
-
-with shelve.open('domain_to_resources_resource_sample_2019-04-12') as db:
-    for k in db:
-        pprint(db[k])
