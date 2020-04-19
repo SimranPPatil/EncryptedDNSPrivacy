70520f578328e8ca00af902fb50089ba3d8d3547
sppatil2 <sppatil2@illinois.edu>
2020-04-19 04:31:24 -0500

    plots for anonset

--- src/plots.py
+++ src/plots.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("python3 file_for_hist")
+    if len(sys.argv) < 6:
+        print("python3 file_for_hist xlabel ylabel title figname")
         exit()
 
     cnts = []
     sizes = []
     file_name = sys.argv[1]
+    xlabel = sys.argv[2]
+    ylabel = sys.argv[3]
+    title = sys.argv[4]
+    figname = sys.argv[5]
     with open(file_name) as f:
         for line in f:
             if "Row" in line:
@@ -21,8 +25,8 @@ if __name__ == "__main__":
 
     print(len(cnts), len(sizes))
     plt.bar(cnts[:100], np.log(sizes)[:100])
-    plt.xlabel("ASN Anonymity set size")
-    plt.ylabel("Log of number of sites with given anonymity set size")
-    plt.title("ASN anonymity set sizes for sites")
-    plt.savefig("asn_anonset.png")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.savefig(figname)
 
