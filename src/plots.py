bfc442a7a83081033958e26c5d5414384d838f67
sppatil2 <sppatil2@illinois.edu>
2020-04-19 04:03:49 -0500

    cnt plot anonset

--- src/plots.py
+++ src/plots.py
@@ -1,20 +1,28 @@
 import sys
 import matplotlib.pyplot as plt
-from os import walk
-
-def plot_file(filename):
-        with open(filename) as curr_file:
-    pass
+import numpy as np
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("python3 graph path_to_sql_output")
+        print("python3 file_for_hist")
         exit()
 
-    out_dir = sys.argv[1]
-    f = []
-    for (dirpath, dirnames, filenames) in walk(mypath):
-        f.extend(filenames)
-        break
-        
-    pass
+    cnts = []
+    sizes = []
+    file_name = sys.argv[1]
+    with open(file_name) as f:
+        for line in f:
+            if "Row" in line:
+                line = line.split(")")[0].split("((")[1].split(", ")
+                cnt = int(line[0])
+                size = int(line[1])
+                cnts.append(cnt)
+                sizes.append(size)
+
+    print(len(cnts), len(sizes))
+    plt.bar(cnts[:100], np.log(sizes)[:100])
+    plt.xlabel("ASN Anonymity set size")
+    plt.ylabel("Log of number of sites with given anonymity set size")
+    plt.title("ASN anonymity set sizes for sites")
+    plt.savefig("asn_anonset.png")
+
